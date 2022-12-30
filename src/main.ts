8228b35a200b9b5c9b427f57f917198d9c82d86d
RafaelKC <rafaelkaua97@gmail.com>
2022-12-30 16:36:46 -0300

    SGVP-B002 added Environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
     );
-  await app.listen(3000);
+
+	const configService = app.get(ConfigService);	
+  const port = configService.get('PORT');
+	
+  await app.listen(port);
 }
 bootstrap();

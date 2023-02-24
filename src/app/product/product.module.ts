da992ad2e3469df80b531cf7c32c54a031e8d963
RafaelKC <rafaelkaua97@gmail.com>
2023-02-23 21:40:20 -0300

    SGVP-13 removed unused import

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,4 +1,4 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Product from './entities/product.entity';
 import { IProductService } from './IProduct.service.interface';
